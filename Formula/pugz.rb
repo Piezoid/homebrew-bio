1a94426d62f38bd1d69dbc57854b907a0c35a293
Maël Kerbiriou <piezo.w.dimd@gmail.com>
2019-05-27 18:37:38 +0000

    travis try

--- Formula/pugz.rb
+++ Formula/pugz.rb
@@ -1,19 +1,12 @@
 class Pugz < Formula
   desc "Truly parallel gzip decompression"
   homepage "https://github.com/Piezoid/pugz"
-  url "https://github.com/Piezoid/pugz/archive/recombseq19-demo.tar.gz"
+  url "https://github.com/Piezoid/pugz/archive/97be6839c48878b498112933655506800af3b8e2.tar.gz"
   version "0.8.0"
-  sha256 "1005519d7ea80c4f59593b0e9bd2e9337c02a2b5c52dfe1732c16f75e4eeecb3"
-
-  depends_on "gcc" if OS.mac? # for openmp
-
-  fails_with :clang # needs openmp
+  sha256 "e0ec4763a9aa64d7ebf333ec333ba72ebfc5797c051a56fd9dd1ce6fca040dc8"
 
   def install
-    # https://github.com/Piezoid/pugz/issues/10
-    inreplace "Makefile", "-lrt", "-lpthread -lrt"
-
-    system "make", "V=1"
+    system "make", "asserts=0", "V=1"
     bin.install "gunzip" => "pugz"
     pkgshare.install "example"
     doc.install "paper/paper.pdf"
